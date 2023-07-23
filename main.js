e73453b779c93c707293eadb54fb5d26c9de461d
felipeMarroquinTepale <felipe.01tepa@gmail.com>
2023-07-23 14:21:28 -0600

    subiendo v2

--- main.js
+++ main.js
@@ -1,149 +1,196 @@
+const nameTask = document.querySelector("#nameTask");
+const form = document.querySelector("#form");
+const tempo = document.getElementById("temporizador");
+const msg_tempo = document.getElementById("msg-tempo");
+const alertDiv = document.getElementById("alert");
 
-//almacenara todas las tareas que puedo ir ejecutando
-const tasks = [];
-//contara la cuenta regresiva
+//nos ayudara si hay una actividad en progreso
+let timer=null;
 let time = 0;
-
-//nos ayudara a ver si hay una actividad en progreso
-let timer = null;
 
 //me señala el tiempo de descanso
 let timerBreack = null;
-
 //me dira la tarea que se esta ejecutando
 let current = null;
 
 
-const bAdd = document.querySelector('#bAdd')
-const itTask = document.querySelector("#itTask");
-const form = document.querySelector("#form");
-const taskName = document.querySelector('#time #taskName');
 
 
-renderTime();
-renderTasks();
+//guardar todas las tareas que se agreguen
+const tasks = []
 
+const agregarTask = async (e) => {
+    try{
+        e.preventDefault();
+        if(nameTask.value != ''){
+            createTask(nameTask.value)
+            nameTask.value = "";
+            renderizarTask();
+        }
 
-form.addEventListener('submit', e=>{
-    e.preventDefault();
-    //Si el input itTask es diferente a vacio me crea una tarea
-    if(itTask.value != ''){
-        createTask (itTask.value);
-        itTask.value = '';
-        // Despues de agregar una nueva tarea tengo que renderizar los elementos que existan en el arreglo tasks
-        renderTasks();
+    }catch(error){
+        console.log(error);
     }
-});
+}
 
-function createTask(value){
-    const newTask  = {
-        id: (Math.random() * 100).toString(36).slice(3),  // id dinamico
-        title:value,
-        completed:false
-    };
-    //agregamos una nueva tarea al arreglo de tasks
+
+function createTask(nameTask){
+    //Creamos un objeto que tendra campos como id, title, isCompleted
+    const newTask = {
+        id: (Math.random() * 100).toString(36).slice(3),  // id dinamico,
+        title: nameTask,
+        isCompleted: false
+    }
+
+    //agregamos el objeto newTask al array tasks
     tasks.unshift(newTask);
 }
 
-function renderTasks(){
-    const html = tasks.map( task => {
-        //cada tarea que tenga en mi arreglo tasks
-        //devuelve dos capas hijas:
-        //1 capa: me dira si la tarea esta completada. Si esta completada me arrojara un mensaje "completado" si no me arrojara un boton "start"
+function renderizarTask(){
+    //imprimimos la vista con cada tarea existente
+    const html = tasks.map(task => {
+    return `
+        <div class="col">
+            <div class="card border-secondary p-3 shadow">
+                <div class="card-body text-center">
+                    <div id="title" class="text-center mt-5">
+                        <strong>Task: </strong>${task.title}
+                    </div>
+                    <div class="mt-5">
+                        <div class="d-grid gap-2">
+                            ${task.isCompleted ?
+                            `
+                            <div class="d-grid gap-2">
+                                <strong>Tarea completada</strong>
+                                <button class="btn btn-success" id="deleteTask" type="submit" data-id="${task.id}">Eliminar tarea</button>
+                            </div>
 
-        return `
-            <div class=task>
-                <div class="completed">${task.completed ? `<span class="done">completado</span>` : `<button class="start-button" data-id="${task.id}">Start</button>` }</div>
-                <div class="title">${task.title}</div>
+                            `
+
+                            :
+
+                            `
+                            <div class="d-grid gap-2">
+                                <button class="btn btn-dark"  id="start-task" data-id="${task.id}">Empezar tarea</button>
+                                <button class="btn btn-success" type="submit" id="deleteTask" data-id="${task.id}">Eliminar tarea</button>
+                            </div>
+                            `
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
-        `;
+        </div>
+    `;
     });
 
-    const tasksContainer = document.querySelector("#tasks");
-    //Me separa cada div con un vacio
-    tasksContainer.innerHTML = html.join('');
 
-    const startButtons = document.querySelectorAll('.task .start-button');
+    const div_tasks = document.querySelector("#tasks");
+    div_tasks.innerHTML = html.join('');
 
-    startButtons.forEach(button => {
+
+    const deleteTask = document.querySelectorAll("#deleteTask");
+    deleteTask.forEach(button=>{
+        button.addEventListener("click",e => {
+            e.preventDefault();
+
+            const id = button.getAttribute("data-id");
+            eliminarTask(id);
+
+        })
+    })
+
+    const startButtonTask = document.querySelectorAll("#start-task");
+    startButtonTask.forEach(button => {
         button.addEventListener("click", e => {
-            //si no hay una actividad en progreso
+            e.preventDefault();
             if(!timer){
                 const id = button.getAttribute("data-id");
-                startButtonHandler(id);
-                button.textContent = "In progress...";
+
+                startConteo(id);
+                button.textContent = "In progress ...";
             }
         })
     });
+
+
+
 }
 
 
-//Tenemos que calcular los 25 minutos de la actividad
-function startButtonHandler(id){
-    //25 minutos y por cada minuto tiene 60 segundos
-    time = 25 * 60;
-    //id actividad actual
-    current = id;
+
+function eliminarTask(id){
     const taskIndex = tasks.findIndex(task => task.id === id);
-    taskName.textContent = tasks[taskIndex].title;
+    tasks.splice(taskIndex, 1);
+    console.log("tasks: ", tasks )
+    renderizarTask();
+}
 
-    renderTime()
 
 
-    timer = setInterval(() => {
-        timeHandler(id);
+function startConteo(id){
+    //time contendra los segundos de 25 minutos
+    time = 25 * 60;
+    current = id;
+
+    renderTimer();
+    const taskIndex = tasks.findIndex(task => task.id === id);
+    alertDiv.classList.add("alert-success");
+    msg_tempo.textContent = "Enfocate con "+ tasks[taskIndex].title;
+
+
+    timer = setInterval (()=>{
+        time --;
+        renderTimer();
+        if(time === 0){
+            clearInterval(timer);
+            markCompleted(id);
+            timer=null;
+            renderizarTask();
+            startBreak();
+        }
     },1000);
-
 }
 
-function timeHandler(id){
-    time --;
-    renderTime();
 
-    if(time ===0 ){
-        clearInterval(timer);
-        markCompleted(id);
-        timer = null;
-        renderTasks();
-        startBreak();
-    }
-}
-
-function renderTime(){
-    const timeDiv = document.querySelector('#time #value');
+function renderTimer(){
+    //convertimos los segundos totales de 25 minutos a minutos otra vez
     const minutes = parseInt(time/60);
-    const seconds = parseInt(time % 60);
-
-    timeDiv.textContent = `${minutes < 10 ? "0" : ""}${minutes} : ${seconds < 10 ? "0" : ""}${seconds}`;
+    //los segundos
+    const seconds = parseInt(time%60);
+    tempo.textContent = `${minutes < 10 ? "0" : ""}${minutes} : ${seconds < 10 ? "0" : ""}${seconds}`;
 }
+
 
 function markCompleted(id){
     const taskIndex = tasks.findIndex((task) => task.id === id);
-    tasks[taskIndex].completed = true;
+    tasks[taskIndex].isCompleted = true;
 }
 
-function startBreak(){
-    time = 3 * 60;
-    taskName.textContent = "Break";
 
-    renderTime();
+function startBreak(){
+    //el tiempo de descanso sera de 3 minutos
+    time = 3 * 60;
+    renderTimer();
+    alertDiv.classList.add("alert-warning");
+    msg_tempo.textContent = "Tiempo de descanso";
 
     timerBreack = setInterval(()=> {
-        timerBreackHandler();
-
+        time --;
+        renderTimer();
+        if(time===0){
+            clearInterval(timerBreack);
+            current = null;
+            timerBreack=null;
+            tempo.textContent = "";
+            msg_tempo.textContent = ""
+            alertDiv.classList.remove("alert-warning");
+            alertDiv.classList.remove("alert-success");
+            renderizarTask();
+        }
     }, 1000);
 }
 
-function timerBreackHandler(){
-    time--;
-    renderTime();
 
-    if(time === 0){
-        clearInterval(timeHandler);
-        current = null;
-        timerBreack=null;
-        taskName.textContent = "";
 
-        renderTasks();
-    }
-}
+form.addEventListener("click",agregarTask);
